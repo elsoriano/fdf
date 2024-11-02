c92f5c44719ab0d8830bebdc0aacd5641a3cd594
rhernand <rhernand@student.42malaga.com>
2024-11-02 21:37:09 +0100

    everything but perspective. colors added

--- src/draw.c
+++ src/draw.c
@@ -6,35 +6,63 @@
 /*   By: rhernand <rhernand@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/01 17:55:33 by rhernand          #+#    #+#             */
-/*   Updated: 2024/11/02 20:03:48 by rhernand         ###   ########.fr       */
+/*   Updated: 2024/11/02 21:35:26 by rhernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/fdf.h"
 #include "../inc/minilibx-linux/mlx.h"
 #include "../inc/libft/inc/libft.h"
+#include <math.h>
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define MOD(a) (a < 0 ? (-a) : (a))
+
+void	ft_persp(float *x, float *y, int z)
+{
+	*x = (*x - *y)* cos(0.8);
+	*y = (*x + *y)* sin(0.8) - (float)z;
+}
+
+int	ft_zoomove(float *x, float *y, t_data *data)
+{
+	int		z;
+	float	move;
+	float	zoom;
+
+	z = data->pts[(int)*y][(int)*x];
+	zoom = 20;
+	*x *= zoom;
+	*y *= zoom;
+	move = 150;
+	*x += move;
+	*y += move;
+	return (z);
+}
 
 void	ft_draw_line(float x, float y, float x1, float y1, t_data *data)
 {
 	float	x_step;
 	float	y_step;
 	float	temp;
-	float	zoom;
+	int		z;
+	int		z1;
+	int		color;
 
-	zoom = 20;
-	x *= zoom;
-	x1 *= zoom;
-	y *= zoom;
-	y1 *= zoom;
+	color = 0xcd5c5c;
+	if (data->pts[(int)y1][(int)x1])
+		color = 0xfffafa;
+	z = ft_zoomove(&x, &y, data);
+	z1 = ft_zoomove(&x1, &y1, data);
 	x_step = x1 - x;
 	y_step = y1 - y;
-	temp = MAX(x_step, y_step);
+	temp = MAX(MOD(x_step), MOD(y_step));
 	x_step /= temp;
 	y_step /= temp;
+	//ft_persp(&x, &y, z);
+	//ft_persp(&x1, &y1, z);
 	while (x != x1 || y != y1)
 	{
-		if (mlx_pixel_put(data->mlx_ses, data->mlx_win, (int)x, (int)y, 0xfffafa) == -1)
+		if (mlx_pixel_put(data->mlx_ses, data->mlx_win, (int)x, (int)y, color) == -1)
 			ft_clean_exit(data, "Error putting pixel");
 		x += x_step;
 		y += y_step;
@@ -55,8 +83,10 @@ void	ft_draw_map(t_data *data)
 		x = 0;
 		while (x < data->cols)
 		{
-			ft_draw_line((float)x, (float)y, (float)x, (float)(y + 1), data);
-			ft_draw_line((float)x, (float)y, (float)(x + 1), (float) y, data);
+			if (y < data->rows - 1)
+				ft_draw_line((float)x, (float)y, (float)x, (float)(y + 1), data);
+			if (x < data->cols - 1)
+				ft_draw_line((float)x, (float)y, (float)(x + 1), (float) y, data);
 			x++;
 		}
 		y++;
