abfabe3ea4b83acb65ac718aab9649641b17704b
rhernand <rhernand@student.42malaga.com>
2024-11-03 20:29:01 +0100

    close button added

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: rhernand <rhernand@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 17:50:27 by rhernand          #+#    #+#             */
-/*   Updated: 2024/11/03 19:30:51 by rhernand         ###   ########.fr       */
+/*   Updated: 2024/11/03 20:27:41 by rhernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,16 @@
 #include "../inc/libft/inc/libft.h"
 #include "../inc/minilibx-linux/mlx.h"
 
+int ft_close(int event)
+{
+	event += 1;
+	exit(EXIT_SUCCESS);
+	return (0);
+}
+
 int	ft_hook(int key, t_data *data)
 {
-	if (key == 65307)
+	if (key == 65307 || key == 64)
 		ft_clean_exit(data, NULL);
 	return (0);
 }
@@ -55,7 +62,8 @@ int	main(int argc, char **argv)
 	ft_set_matrix(argv[1], data);
 	ft_draw_map(data);
 	mlx_key_hook(data->mlx_win, ft_hook, data);
+	mlx_hook(data->mlx_win, 17, 0, ft_close, NULL);
 	mlx_loop(data->mlx_ses);
-	//ft_clean_exit(data, NULL);
+	// ft_clean_exit(data, NULL);
 	return (0);
 }
